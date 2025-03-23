c1096ba13d03a81acb6f4259d32e291b9e63d277
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2025-03-23 19:12:39 +0100

    fix: added svelte plugin to prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -7,7 +7,7 @@ const config = {
     tabWidth: 4,
     semi: true,
     singleQuote: false,
-    plugins: ["prettier-plugin-tailwindcss"],
+    plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-svelte"],
 };
 
 module.exports = config;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
+    import PresenceTable from "./presence-table-tab.svelte";
     import { Separator } from "$lib/components/ui/separator";
     import * as Tabs from "$lib/components/ui/tabs/index.js";
 </script>
 
-<Tabs.Root value="presence" class="w-full flex flex-col">
+<Tabs.Root value="presence" class="w-full h-screen flex flex-col">
     <Tabs.List>
         <Tabs.Trigger value="presence">Присутност</Tabs.Trigger>
-        <Tabs.Trigger value="agenda">Дневни ред</Tabs.Trigger>
+        <Tabs.Trigger value="voting">Гласање</Tabs.Trigger>
     </Tabs.List>
     <Separator />
-    <Tabs.Content value="presence">
-        Присутност
+    <Tabs.Content value="presence" class="mt-0 h-full">
+        <PresenceTable />
     </Tabs.Content>
-    <Tabs.Content value="agenda">
-        Дневни ред
-    </Tabs.Content>
+    <Tabs.Content value="voting">Гласање</Tabs.Content>
 </Tabs.Root>
