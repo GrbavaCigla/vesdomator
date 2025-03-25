f94f3e4fe2563253fab3e5b8757ccd1a6c84df9c
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2025-03-26 00:48:57 +0100

    feat: updated remote config url to point to repo raw file

--- src-tauri/src/global.rs
+++ src-tauri/src/global.rs
@@ -1,2 +1,2 @@
-pub const REMOTE_CONFIG_URI: &str = "https://gist.githubusercontent.com/GrbavaCigla/1cf7fbc4298b9b6f1eec3609d4f62742/raw/bdefbf7a8a48999b412817b72037071211e3e9fa/gistfile1.txt";
+pub const REMOTE_CONFIG_URI: &str = "https://raw.githubusercontent.com/GrbavaCigla/vesdomator/refs/heads/main/src-tauri/config.toml";
 pub const FALLBACK_CONFIG: &str = include_str!("../config.toml");
