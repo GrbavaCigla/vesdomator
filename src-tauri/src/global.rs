0b1183c4b0e36690771e63cca7cd2c370df93248
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2025-03-26 01:08:44 +0100

    chore: renamed project name

--- src-tauri/src/global.rs
+++ src-tauri/src/global.rs
@@ -1,2 +1,2 @@
-pub const REMOTE_CONFIG_URI: &str = "https://raw.githubusercontent.com/GrbavaCigla/vesdomator/refs/heads/main/src-tauri/config.toml";
+pub const REMOTE_CONFIG_URI: &str = "https://raw.githubusercontent.com/GrbavaCigla/vsd-o-mator/refs/heads/main/src-tauri/config.toml";
 pub const FALLBACK_CONFIG: &str = include_str!("../config.toml");
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    vesdomator_lib::run()
+    vsd_o_mator_lib::run()
 }
